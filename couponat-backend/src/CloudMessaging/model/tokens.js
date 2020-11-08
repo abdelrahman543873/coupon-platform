d737daa010289c302c3cc93ee21b214fa3f65805
Abdo_123 <abdulrhmanadel60@gmail.com>
2020-11-08 14:32:10 +0200

    asda

--- couponat-backend/src/CloudMessaging/model/tokens.js
+++ couponat-backend/src/CloudMessaging/model/tokens.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
 
-let tokensSchema = mongoose.Schema({
-  fcmToken: String,
-});
+let tokensSchema = mongoose.Schema(
+  {
+    fcmToken: String,
+  },
+  {
+    versionKey: false,
+  }
+);
 
 let TokensModel = mongoose.model("Tokens", tokensSchema);
 
