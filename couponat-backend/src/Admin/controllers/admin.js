dfbe4d33b06456afc7f84ae1c13cf5539d6db880
Abdo_123 <abdulrhmanadel60@gmail.com>
2020-11-11 11:59:41 +0200

    d

--- couponat-backend/src/Admin/controllers/admin.js
+++ couponat-backend/src/Admin/controllers/admin.js
@@ -162,9 +162,14 @@ const AdminsController = {
 
   async getProviders(req, res, next) {
     let providers = await ProviderModule.getAll();
-    providers = providers.map((provider) => {
-      return new Provider(provider);
-    });
+    for (let i = 0; i < providers.length; i++) {
+      providers[i] = new Provider(providers[i]);
+      let totalCount = await ProviderModule.getStatistics(providers[i].id);
+      providers[i] = {
+        provider: providers[i],
+        totalCount,
+      };
+    }
     return res.status(200).send({
       isSuccessed: true,
       data: providers,
