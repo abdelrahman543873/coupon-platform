0ac180c5d43eef4e9b4e8d1dd3e20a0e70195f74
Abdo_123 <abdulrhmanadel60@gmail.com>
2020-11-23 15:28:24 +0200

    l

--- couponat-backend/src/Users/middlewares/validations/provider.js
+++ couponat-backend/src/Users/middlewares/validations/provider.js
@@ -5,8 +5,8 @@ const ProviderValidationWares = {
   add(req, res, next) {
     console.log("midille");
     console.log(JSON.stringify(req.body));
-    //req.body= JSON.parse(req.body);
-    if (!req.body.location) {
+    let location = [];
+    if (req.body.lat.length != req.body.long.length) {
       let lang = req.headers.lang || "ar",
         errMsg =
           lang == "en"
@@ -14,6 +14,23 @@ const ProviderValidationWares = {
             : "يجب اختيار موقع لكل مدينه";
       return next(boom.badData(errMsg));
     }
+    for (let i = 0; h < req.body.lat.length; i++) {
+      location.push({
+        lat: req.body.lat[i],
+        long: req.body.long[i],
+      });
+    }
+    req.body.location = location;
+    delete req.body.lat;
+    delete req.body.long;
+    // if (!req.body.location) {
+    //   let lang = req.headers.lang || "ar",
+    //     errMsg =
+    //       lang == "en"
+    //         ? "Evry City Must have specific location!"
+    //         : "يجب اختيار موقع لكل مدينه";
+    //   return next(boom.badData(errMsg));
+    // }
     if (req.body.cities.length != req.body.location.length) {
       let lang = req.headers.lang || "ar",
         errMsg =
