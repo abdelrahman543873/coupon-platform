ce1a3434966825f4f42abba52941662bb734619e
Abdo_123 <abdulrhmanadel60@gmail.com>
2020-11-26 15:07:45 +0200

    d

--- couponat-backend/src/Users/middlewares/validations/provider.js
+++ couponat-backend/src/Users/middlewares/validations/provider.js
@@ -69,11 +69,7 @@ const ProviderValidationWares = {
     console.log(JSON.stringify(req.body));
     console.log(req.body);
 
-    if (
-      (req.body.cities && (!req.body.lat || !req.body.long)) ||
-      req.body.lat.length != req.body.long.length ||
-      req.body.lat.length != req.body.cities.length
-    ) {
+    if (req.body.cities && (!req.body.lat || !req.body.long)) {
       let lang = req.headers.lang || "ar",
         errMsg =
           lang == "en"
@@ -81,19 +77,19 @@ const ProviderValidationWares = {
             : "يجب اختيار موقع لكل مدينه";
       return next(boom.badData(errMsg));
     }
-    // if (r) {
-    //   if (
-    //     req.body.lat.length != req.body.long.length ||
-    //     req.body.lat.length != req.body.cities.length
-    //   ) {
-    //     let lang = req.headers.lang || "ar",
-    //       errMsg =
-    //         lang == "en"
-    //           ? "Every City Must have specific location!"
-    //           : "يجب اختيار موقع لكل مدينه";
-    //     return next(boom.badData(errMsg));
-    //   }
-    // }
+    if (req.body.cities && req.body.lat && req.body.long) {
+      if (
+        req.body.lat.length != req.body.long.length ||
+        req.body.lat.length != req.body.cities.length
+      ) {
+        let lang = req.headers.lang || "ar",
+          errMsg =
+            lang == "en"
+              ? "Every City Must have specific location!"
+              : "يجب اختيار موقع لكل مدينه";
+        return next(boom.badData(errMsg));
+      }
+    }
 
     if (req.body.cities) {
       let location = [];
