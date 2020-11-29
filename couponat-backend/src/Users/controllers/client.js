19ca0a625edc8694ad73429d86b72fd5818bf3e6
Abdo_123 <abdulrhmanadel60@gmail.com>
2020-11-29 11:01:47 +0200

    d

--- couponat-backend/src/Users/controllers/client.js
+++ couponat-backend/src/Users/controllers/client.js
@@ -375,11 +375,14 @@ const ClientControllers = {
       return coupon;
     });
 
-    for (let i = 0; i < coupons; i++) {
-      if (coupons[i].totalCount < 1) {
-        coupons.splice(i, 1);
-      }
-    }
+    coupons = coupons.filter((coupon) => {
+     return coupon.totalCount!=0;
+    });
+    // for (let i = 0; i < coupons; i++) {
+    //   if (coupons[i].totalCount < 1) {
+    //     coupons.slice(i, 1);
+    //   }
+    // }
     for (let i = 0; i < coupons.length; i++) {
       let sub = await subscriptionModule.getUserSubscripe(
         userId,
