990cd798c3ea58d3f18c13de1f158de68edc0780
KhalifaGad <kkhalifa.gad@gmail.com>
2021-03-30 13:37:47 -0200

    db fix

--- app.js
+++ app.js
@@ -6,15 +6,16 @@ import cron from "node-cron";
 import dotenv from "dotenv";
 
 dotenv.config();
-let dbUrl = process.env.RUN_INSIDE_DOCKER
-  ? process.env.COUPONAT_DB_URL_COMPOSE
-  : process.env.COUPONAT_DB_URL_LOCAL;
-console.log(dbUrl);
-
-//if (process.env.RUN_INSIDE_DOCKER) {
-//  dbUrl = process.env.COUPONAT_DB_URL_COMPOSE;
-//}
+//let dbUrl = process.env.RUN_INSIDE_DOCKER
+//  ? process.env.COUPONAT_DB_URL_COMPOSE
+//  : process.env.COUPONAT_DB_URL_LOCAL;
 //console.log(dbUrl);
+
+let dbUrl = process.env.COUPONAT_N_PORT;
+if (process.env.RUN_INSIDE_DOCKER) {
+  dbUrl = process.env.COUPONAT_DB_URL_COMPOSE;
+}
+console.log(dbUrl);
 
 
 connectDB(dbUrl)
