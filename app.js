04c191e474446123db884ac4631e76c5e1315408
KhalifaGad <kkhalifa.gad@gmail.com>
2021-03-30 13:46:08 -0200

    db fix

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 let dbUrl = process.env.RUN_INSIDE_DOCKER
-  ? process.env.COUPONAT_DB_URL_COMPOSE;
+ // ? 
+:process.env.COUPONAT_DB_URL_COMPOSE;
 //  : process.env.COUPONAT_DB_URL_LOCAL;
 console.log(dbUrl);
 
