714182259ee098ea0f3181b73e96b227b175a842
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2021-03-31 15:18:17 +0200

    refactor of provider reigster

--- src/provider/inputs/provider-register.input.js
+++ src/provider/inputs/provider-register.input.js
@@ -1,11 +1,11 @@
 import Joi from "joi";
 export const ProviderRegisterInput = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
-  email: Joi.string().email().lowercase().required(),
+  name: Joi.string().min(3).max(30),
+  email: Joi.string().email().lowercase(),
   phone: Joi.string().min(7),
-  password: Joi.string().min(8).required(),
-  slogan: Joi.string().min(10).required(),
-  officeTele: Joi.string().min(3).required(),
+  password: Joi.string().min(8),
+  slogan: Joi.string().min(10),
+  officeTele: Joi.string().min(3),
   websiteLink: Joi.string().allow("").min(3).optional(),
   facebookLink: Joi.string().allow("").min(3).optional(),
   instagramLink: Joi.string().min(3).allow("").optional(),
