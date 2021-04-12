2a1187b372da00b95f334b864518ea9075e0f553
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2021-04-12 14:32:29 +0200

    refactor: for testing service

--- src/testing/testing.service.js
+++ src/testing/testing.service.js
@@ -1,12 +1,13 @@
 import { UserRoleEnum } from "../user/user-role.enum.js";
-import { userFactory } from "../user/user.factory.js";
+import { buildUserParams, userFactory } from "../user/user.factory.js";
 import { generateToken } from "../utils/JWTHelper.js";
 
 export const getAdminTokenService = async (req, res, next) => {
   try {
-    const user = await userFactory({ role: UserRoleEnum[2] });
+    const params = await buildUserParams({ role: UserRoleEnum[2] });
+    const user = await userFactory(params);
     res.status(200).json({
-      data: user,
+      data: params,
       authToken: generateToken(user.id, "ADMIN"),
     });
   } catch (error) {
