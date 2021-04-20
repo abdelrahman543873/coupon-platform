593c584f8b8a517905cac23c6143fb3f76a87b63
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2021-04-20 20:58:39 +0200

    added testing for isSubscribe and isUsed

--- test/customer/get-customer-subscriptions.spec.js
+++ test/customer/get-customer-subscriptions.spec.js
@@ -1,9 +1,15 @@
 import { testRequest } from "../request.js";
-import { providerCustomerCouponsFactory } from "../../src/coupon/coupon.factory.js";
+import {
+  providerCustomerCouponFactory,
+  providerCustomerCouponsFactory,
+} from "../../src/coupon/coupon.factory.js";
 import { HTTP_METHODS_ENUM } from "../request.methods.enum.js";
 import { rollbackDbForCustomer } from "./rollback-for-customer.js";
 import { customerFactory } from "../../src/customer/customer.factory.js";
-import { GET_CUSTOMER_SUBSCRIPTIONS } from "../endpoints/customer.js";
+import {
+  GET_CUSTOMER_SUBSCRIPTIONS,
+  MARK_COUPON_USED,
+} from "../endpoints/customer.js";
 describe("get customers subscriptions suite case", () => {
   afterEach(async () => {
     await rollbackDbForCustomer();
@@ -30,5 +36,37 @@ describe("get customers subscriptions suite case", () => {
     expect(res.body.data.docs[0].paymentType._id).toBeTruthy();
     expect(res.body.data.docs[0].coupon.provider._id).toBeTruthy();
     expect(res.body.data.docs.length).toBe(10);
+  });
+
+  it("isSubscribe should evaluate to false when isUsed is true", async () => {
+    const customer = await customerFactory();
+    const subscription = await providerCustomerCouponFactory(
+      {},
+      { customer: customer.user },
+      {}
+    );
+    const res = await testRequest({
+      method: HTTP_METHODS_ENUM.GET,
+      url: GET_CUSTOMER_SUBSCRIPTIONS,
+      token: customer.token,
+    });
+    expect(res.body.data.docs[0].coupon.isSubscribe).toBe(true);
+    expect(res.body.data.docs[0].coupon._id).toBeTruthy();
+    expect(res.body.data.docs[0].customer._id).toBeTruthy();
+    expect(res.body.data.docs[0].paymentType._id).toBeTruthy();
+    expect(res.body.data.docs[0].coupon.provider._id).toBeTruthy();
+    expect(res.body.data.docs.length).toBe(1);
+    await testRequest({
+      method: HTTP_METHODS_ENUM.POST,
+      url: MARK_COUPON_USED,
+      variables: { coupon: subscription.coupon },
+      token: customer.token,
+    });
+    const res2 = await testRequest({
+      method: HTTP_METHODS_ENUM.GET,
+      url: GET_CUSTOMER_SUBSCRIPTIONS,
+      token: customer.token,
+    });
+    expect(res2.body.data.docs[0].coupon.isSubscribe).toBe(false);
   });
 });
