ea16183d0a2958956126aa18f39b5c91bb5d0441
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2021-04-20 10:11:55 +0200

    added isfav ,isSbuscribe to search

--- src/search/search.service.js
+++ src/search/search.service.js
@@ -1,15 +1,32 @@
-import { searchCouponsRepository } from "../coupon/coupon.repository.js";
+import { getCustomerSubscribedCoupons, searchCouponsRepository } from "../coupon/coupon.repository.js";
+import { getCustomerRepository } from "../customer/customer.repository.js";
 import { searchProvidersRepository } from "../user/user.repository.js";
 import { BaseHttpError } from "../_common/error-handling-module/error-handler.js";
 
 export const searchCouponsService = async (req, res, next) => {
   try {
     if (!req.query.name) throw new BaseHttpError(620);
+    const subscriptionsIds = [];
+    const favCoupons = [];
+    if (req.currentUser) {
+      const subscribedCoupons = await getCustomerSubscribedCoupons(
+        req.currentUser._id
+      );
+      subscribedCoupons.forEach((coupon) => {
+        subscriptionsIds.push(coupon.coupon);
+      });
+      const customer = await getCustomerRepository(req.currentUser._id);
+      customer.favCoupons.forEach((coupon) => {
+        favCoupons.push(coupon);
+      });
+    }
     const searchResult = await searchCouponsRepository(
       req.query.name,
       req.query.offset,
       req.query.limit,
-      req.query.category
+      req.query.category,
+      subscriptionsIds,
+      favCoupons
     );
     res.status(200).json({
       success: true,
