fedd2625670bcb805f2d9ae750f0667375f542a2
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2021-04-21 11:28:50 +0200

    fix: social register

--- src/customer/inputs/social-register.input.js
+++ src/customer/inputs/social-register.input.js
@@ -2,8 +2,10 @@ import Joi from "joi";
 import { socialMediaEnum } from "../social-media-type.enum.js";
 export const SocialRegisterInput = Joi.object({
   name: Joi.string().min(3).max(30),
-  socialMediaId: Joi.string(),
-  socialMediaType: Joi.string().valid(...socialMediaEnum),
+  socialMediaId: Joi.string().required(),
+  socialMediaType: Joi.string()
+    .valid(...socialMediaEnum)
+    .required(),
   phone: Joi.string().regex(/\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/),
   email: Joi.string().email().lowercase(),
   profilePictureURL: Joi.string(),
