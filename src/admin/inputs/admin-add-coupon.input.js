ce26734dfe786443a210f991939c3ed62a20b1e3
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2021-04-27 12:05:14 +0200

    fix: admin coupon input

--- src/admin/inputs/admin-add-coupon.input.js
+++ src/admin/inputs/admin-add-coupon.input.js
@@ -1,13 +1,21 @@
 import Joi from "joi";
 
 export const AdminAddCouponInput = Joi.object({
-  enName: Joi.string().min(3),
-  arName: Joi.string().min(3),
-  enDescription: Joi.string().min(3),
-  arDescription: Joi.string().min(3),
-  servicePrice: Joi.number().positive().precision(2),
-  offerPrice: Joi.number().positive().precision(2),
-  category: Joi.string().regex(/^[0-9a-fA-F]{24}$/),
-  provider: Joi.string().regex(/^[0-9a-fA-F]{24}$/),
-  amount: Joi.number().min(0).max(9999),
+  enName: Joi.string().min(3).required(),
+  arName: Joi.string().min(3).required(),
+  enDescription: Joi.string().min(3).required(),
+  arDescription: Joi.string().min(3).required(),
+  servicePrice: Joi.number()
+    .positive()
+    .precision(2)
+    .greater(Joi.ref("offerPrice"))
+    .required(),
+  offerPrice: Joi.number().positive().precision(2).required(),
+  category: Joi.string()
+    .regex(/^[0-9a-fA-F]{24}$/)
+    .required(),
+  amount: Joi.number().min(0).max(9999).required(),
+  provider: Joi.string()
+    .regex(/^[0-9a-fA-F]{24}$/)
+    .required(),
 });
