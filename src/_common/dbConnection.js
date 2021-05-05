434aa74e8000d53af6f0e1f595cdd4e175be6d3b
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2021-05-05 14:32:57 +0200

    db connection

--- src/_common/dbConnection.js
+++ src/_common/dbConnection.js
@@ -3,11 +3,10 @@ import mongoose from "mongoose";
 // connectDB: responsible for database connection using mongoose,
 // @param(dbURL): url for database
 export const connectDB = async (dbURL) => {
-  const mongo = await mongoose.connect(dbURL, {
+  return await mongoose.connect(dbURL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
   });
-  return mongo;
 };
