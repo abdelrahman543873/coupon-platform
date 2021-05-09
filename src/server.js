7468e0fbd22d8692e93fda7306d2508c541a45f8
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2021-05-09 10:09:02 +0200

    modified rate limit

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ const corsOptions = {
 const swaggerSpec = swaggerJSDoc(options);
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 1000, // limit each IP to 100 requests per windowMs
 });
 server.use(responseTime());
 server.use(limiter);
