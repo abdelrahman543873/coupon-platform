5b94cd9112b7696f62bc2d813639a2df303df323
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2021-05-16 16:09:13 +0200

    terms and conditions test refactor

--- test/terms-and-conditions/get-terms-and-conditions.spec.js
+++ test/terms-and-conditions/get-terms-and-conditions.spec.js
@@ -11,39 +11,39 @@ describe("get terms and conditions suite case", () => {
     await rollbackDbForTermsAndConditions();
   });
   it("get terms and conditions", async () => {
-    await termsAndConditionsFactory(10, { key: TermsAndConditionsEnum[0] });
+    await termsAndConditionsFactory(1, { key: TermsAndConditionsEnum[0] });
     const res = await testRequest({
       method: HTTP_METHODS_ENUM.GET,
       url: GET_TERMS_AND_CONDITIONS,
     });
-    expect(res.body.data.length).toBe(10);
+    expect(res.body.data.length).toBe(1);
   });
 
   it("get customer terms and conditions", async () => {
     const customer = await customerFactory();
-    await termsAndConditionsFactory(10, { key: TermsAndConditionsEnum[0] });
+    await termsAndConditionsFactory(1, { key: TermsAndConditionsEnum[0] });
     const res = await testRequest({
       method: HTTP_METHODS_ENUM.GET,
       url: GET_TERMS_AND_CONDITIONS,
       token: customer.token,
     });
-    expect(res.body.data.length).toBe(10);
+    expect(res.body.data.length).toBe(1);
   });
 
   it("get provider terms and conditions", async () => {
     const provider = await providerFactory();
-    await termsAndConditionsFactory(10, { key: TermsAndConditionsEnum[1] });
+    await termsAndConditionsFactory(1, { key: TermsAndConditionsEnum[1] });
     const res = await testRequest({
       method: HTTP_METHODS_ENUM.GET,
       url: GET_TERMS_AND_CONDITIONS,
       token: provider.token,
     });
-    expect(res.body.data.length).toBe(10);
+    expect(res.body.data.length).toBe(1);
   });
 
   it("get correct provider terms and conditions", async () => {
     const provider = await providerFactory();
-    await termsAndConditionsFactory(10, { key: TermsAndConditionsEnum[0] });
+    await termsAndConditionsFactory(1, { key: TermsAndConditionsEnum[0] });
     const res = await testRequest({
       method: HTTP_METHODS_ENUM.GET,
       url: GET_TERMS_AND_CONDITIONS,
