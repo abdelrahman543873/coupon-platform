9acf3969cff61c329ae9c5afdfbc516df6bbd09c
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2021-05-18 12:07:58 +0200

    removed terms and conditions limitation

--- src/terms-and-conditions/inputs.ts/add-terms-and-conditions.input.js
+++ src/terms-and-conditions/inputs.ts/add-terms-and-conditions.input.js
@@ -2,8 +2,8 @@ import Joi from "joi";
 import { TermsAndConditionsEnum } from "../terms-and-conditions.enum.js";
 
 export const AddTermsAndConditionsInput = Joi.object().keys({
-  enDescription: Joi.string().min(3).max(255).required(),
-  arDescription: Joi.string().min(3).max(255).required(),
+  enDescription: Joi.string().min(3).required(),
+  arDescription: Joi.string().min(3).required(),
   key: Joi.string()
     .valid(...TermsAndConditionsEnum)
     .required(),
--- src/terms-and-conditions/inputs.ts/update-terms-and-conditions.input.js
+++ src/terms-and-conditions/inputs.ts/update-terms-and-conditions.input.js
@@ -4,7 +4,7 @@ export const UpdateTermsAndConditionsInput = Joi.object().keys({
   termsAndConditions: Joi.string()
     .regex(/^[0-9a-fA-F]{24}$/)
     .required(),
-  enDescription: Joi.string().min(3).max(255),
-  arDescription: Joi.string().min(3).max(255),
+  enDescription: Joi.string().min(3),
+  arDescription: Joi.string().min(3),
   key: Joi.string().valid(...TermsAndConditionsEnum),
 });
