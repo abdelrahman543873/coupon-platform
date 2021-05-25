e14878097a22a500c399df64171bcace2f6fbacc
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2021-05-25 17:30:08 +0200

    console.log() adding

--- src/coupon/coupon.service.js
+++ src/coupon/coupon.service.js
@@ -21,6 +21,7 @@ export const adminAddCouponService = async (req, res, next) => {
       ...req.body,
       logoURL: req?.file,
     });
+    console.log(coupon);
     const couponDetails = await getCoupon({ _id: coupon._id });
     return res.status(200).json({
       success: true,
