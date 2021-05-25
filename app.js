8e0f6affe8b3de6bf35393f97e665f1259d3c380
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2021-05-25 13:31:55 +0200

    rfactor

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ import dotenv from "dotenv";
 import admin from "firebase-admin";
 import { serviceAccountConfig } from "./src/notification/service-account-file.js";
 import { connectDB } from "./src/_common/dbConnection.js";
-import { ProviderModel } from "./src/provider/models/provider.model.js";
-import { termsAndConditionsModel } from "./src/terms-and-conditions/models/terms-and-conditions.model.js";
 dotenv.config();
 
 let dbUrl = process.env.RUN_INSIDE_DOCKER
